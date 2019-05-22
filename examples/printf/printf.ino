4a237802e7c8312c1d632c2e089507346c0f1b81
quentin <vey.quentin@gmail.com>
2019-05-22 14:05:31 +0200

    Improves example.

--- examples/printf/printf.ino
+++ examples/printf/printf.ino
@@ -15,7 +15,9 @@ void setup() {
 int i=1;
 uint32_t j=1;
 uint64_t k=1;
-float p=M_PI;
+float p=1;
+double d=1;
+
 
 void loop() {
   printf("\n");
@@ -25,9 +27,14 @@ void loop() {
   printf("\n");
   printf("This is an int : %d.\n", i?i*=-2:++i);
   printf("This is an uint32_t printed with %%"PRIu32": %"PRIu32".\n", j?j*=2:++j);
-  printf("This is an uint64_t printed whith %%"PRIu64" : %"PRIu64".\n", k?k*=2:++k);
-  printf("This is an uint64_t printed whith %%"PRIu32" : %"PRIu32".\n", k);
-  printf("This is an float : %f\n", p*=10);
+  printf("This is an uint64_t printed with %%"PRIu64" : %"PRIu64".\n", k?k*=2:++k);
+  printf("This is an uint64_t printed with %%"PRIu32" : %"PRIu32".\n", k);
+  char buffer[20];
+  printf("This is an uint64_t printed in decimal with ulltoa : %s.\n", ulltoa(k,buffer,sizeof(buffer),10));
+  printf("This is an uint64_t printed in hexadecimal with ulltoa : %s.\n", ulltoa(k,buffer,sizeof(buffer),16));
+  printf("This is a float   : %f\n", M_PI*(p*=10));
+  printf("This is a double  : %f\n", M_PI*(d*=10));
+  printf("(Should look like : 314159265358979323846...)\n");
   char *boatTop="_/\\_";
   char *boatBottom="\\__/";
   printf("This boat is afloat ~~~~~~~%s~~~~~~~\n",boatTop);
